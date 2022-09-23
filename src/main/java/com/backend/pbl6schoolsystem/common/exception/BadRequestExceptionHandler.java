94f430c53ef63f52da9798b6d23b9657df3dde4c
ngdangkiet <kietnguyen17052001@gmail.com>
2022-09-23 14:01:21 +0700

    feature exception handler

---
+++ src/main/java/com/backend/pbl6schoolsystem/common/exception/BadRequestExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.backend.pbl6schoolsystem.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.sql.Timestamp;
+
+@ControllerAdvice
+public class BadRequestExceptionHandler {
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<Object> handler(NotFoundException notFoundException) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ApiException apiException = new ApiException(
+                notFoundException.getMessage(),
+                status,
+                new Timestamp(System.currentTimeMillis())
+        );
+        return new ResponseEntity<>(apiException, status);
+    }
+}
