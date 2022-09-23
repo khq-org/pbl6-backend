c7a6e3369c1382d233381e494e0cca938f3d2096
ngdangkiet <kietnguyen17052001@gmail.com>
2022-09-23 14:40:23 +0700

    constants and enums day 1

---
+++ src/main/java/com/backend/pbl6schoolsystem/common/constant/Constants.java
@@ -0,0 +1,10 @@
+package com.backend.pbl6schoolsystem.common.constant;
+
+public class Constants {
+    public static final String ADMIN_ROLE = "ADMIN";
+    public static final String SCHOOL_ROLE = "SCHOOL";
+    public static final String TEACHER_ROLE = "TEACHER";
+    public static final String STUDENT_ROLE = "STUDENT";
+
+
+}
