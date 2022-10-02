c2fcde1fbc5db5dbf8d4fa5bb1020c576baf0555
ngdangkiet <kietnguyen17052001@gmail.com>
2022-10-02 09:56:56 +0700

    fix querydsl

--- src/main/java/com/backend/pbl6schoolsystem/Pbl6SchoolsystemApplication.java
+++ src/main/java/com/backend/pbl6schoolsystem/Pbl6SchoolsystemApplication.java
@@ -2,8 +2,11 @@ package com.backend.pbl6schoolsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@ComponentScan({"com.querydsl.jpa.impl"})
 public class Pbl6SchoolsystemApplication {
 
 	public static void main(String[] args) {
